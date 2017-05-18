22b2f56c8564b7c8910c394112e20567311da96e
ShotaKitazawa <skitazawa1121@gmail.com>
2017-05-18 15:05:25 +0900

    class5: finish

--- class5/quick_sort.c
+++ class5/quick_sort.c
@@ -15,40 +15,27 @@ void swap(int *a, int *b) {
 }
 
 void quick_sort(int list[], int low, int high, int num) {
-  int pivot, pivot_index, low_index, high_index;
-  printf("low %d, high %d, ", low,high);
+  int pivot, low_index, high_index;
   if (low < high) {
-    pivot_index = (high+low)/2;
-    pivot = list[pivot_index];
+    pivot = list[(low + high) / 2];
     low_index = low;
     high_index = high;
-    printf("pivot %d, pivot_index %d\n",pivot, pivot_index);
 
+    swap(&list[(low + high) / 2], &list[low]);
     while (low_index < high_index) {
-      //printf("%d",high_index);
-      while (list[low_index] <= pivot && low_index < pivot_index) {
+      while (list[low_index] <= pivot && low_index <= high) {
         low_index++;  // from low,  find an element >  pivot
       }
-      while (list[high_index] > pivot && high_index > pivot_index) {
+      while (list[high_index] > pivot && high_index >= low) {
         high_index--;  // from high, find an element <= pivot
       }
       if (low_index < high_index) {  // two elements is in wrong order
-      //printf("%d\n",list[low_index]);
         swap(&list[low_index], &list[high_index]);  // swap two elements
-        if (low_index == pivot_index) {
-          pivot_index = high_index;
-          low_index++;
-        }
-        if (high_index == pivot_index) {
-          pivot_index = low_index;
-          high_index--;
-        }
       }
     }                                     // all elements are checked
-    //swap(&list[low], &list[high_index]);  // put pivot in final position
-    printf("high_index %d, low_index %d, ", pivot,high, low,high_index,low_index);
-    array_print("swap", list, num);
+    swap(&list[low], &list[high_index]);  // put pivot in final position
 
+    array_print("swap", list, num);
     quick_sort(list, low, high_index - 1, num);   // sort low  part
     quick_sort(list, high_index + 1, high, num);  // sort high part
   }
